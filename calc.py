5fb05a74a9b1b62d18a595869d9fcc3f5b4806cb
Ana-gon03 <lupita.ortiz.0311@gmail.com>
2025-02-20 22:00:41 -0600

    Update calc.py
    Funcion resta

--- calc.py
+++ calc.py
@@ -22,7 +22,6 @@ def raiz():
 
 f = 1
 while f == 1:
-    opcion = 1
     print("Menu de calculadora")
     print("1. Suma")
     print("2. Resta")
@@ -31,7 +30,7 @@ while f == 1:
     print("5. Potencia")
     print("6. Raiz")
     print("7. Salir")
-    input(opcion)
+    opcion = int(input("ingresa la opcion\n"))
     if(opcion == 1):
         sumar()
     elif(opcion == 2):
@@ -44,8 +43,6 @@ while f == 1:
         potencia()
     elif(opcion == 6):
         raiz()
-    elif(opcion == 6):
+    elif(opcion == 7):
         f = 0
-    else:
-        print("Opcion invalida")
     
